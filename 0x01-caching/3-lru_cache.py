d875e080c9e5087582584fcc77145fb3525230d6
dalhatmd <umarwalid4055@gmail.com>
2024-10-29 15:18:20 +0100

    Add task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" LRU cache """
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    def __init__(self):
+        """ initialises using an OrderedDict"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    """ LRU cache implementation """
+    def get(self, key):
+        """ gets an item from cache """
+        if key is None or key not in self.cache_data:
+            return
+
+        # move the last gotten item to the end
+        self.cache_data.move_to_end(key)
+
+        return self.cache_data[key]
+
+    def put(self, key, item):
+        """ adds an item to cache """
+        if key is None or item is None:
+            return None
+
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            least_item = self.cache_data.popitem(last=False)
+            print(f"DISCARD {least_item[0]}")
