e89f8a310b8845ec18680a2859233709828984ca
dalhatmd <umarwalid4055@gmail.com>
2024-10-29 08:56:22 +0100

    Add task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" LIFO cache """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFO Cache implementation"""
+    def put(self, key, item):
+        """ adds an item to cache"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) + 1 > self.MAX_ITEMS:
+            #+ 1 to check if it would be greater after adding
+            last_item = self.cache_data.popitem()
+            print(f"DISCARD: {last_item[0]}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ gets an item from cache """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
