dafcc74969619adb76a620e2c6e2275764601757
dalhatmd <umarwalid4055@gmail.com>
2024-11-01 16:24:11 +0100

    change algorithm

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,30 +1,29 @@
-#!/usr/bin/env python3
-""" MRU Cache """
-from collections import OrderedDict
-BaseCaching = __import__('base_caching').BaseCaching
+#!/usr/bin/python3
+""" MRU cache implementation """
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRU Cache implementation"""
+    """MRU Cache implementation"""
+
     def __init__(self):
         """initialiser"""
         super().__init__()
-        self.cache_data = OrderedDict()
-
-    def get(self, key):
-        """ gets an item from cache"""
-        if key is None or key not in self.cache_data:
-            return
-        self.cache_data.move_to_end(key)
-        return self.cache_data[key]
+        self.order = []
 
     def put(self, key, item):
-        """ put function """
-        if key is None or item is None:
-            return None
-        
-        self.cache_data[key] = item
+        """put function"""
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                removed = self.order.pop()
+                self.cache_data.pop(removed)
+                print(f"DISCARD: {removed}")
+            self.cache_data[key] = item
+            self.order.append(key)
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            most_used = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {most_used[0]}")
+    def get(self, key):
+        """get method"""
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data.get(key)
